79e2905d0e255f12058b87e8f80ab840bc204021
Hafiz Chand <chandsethi970@gmail.com>
2020-04-03 04:53:21 -0700

    Rang Selector

---
+++ main.js
@@ -0,0 +1,50 @@
+
+const sliderContainer = document.querySelector('.slider-container');
+const trackProgress = document.querySelector('.track-progress');
+const thumb = document.querySelector('.thumb');
+const thumbIndicator = document.querySelector('.thumb .thumb-indicator');
+const sliderContainerStart = sliderContainer.offsetLeft;
+const sliderContainerWidth = sliderContainer.offsetWidth;
+var translate;
+var dragging = false;
+var percentage = 14;
+
+document.addEventListener('mousedown', function(e) {
+  if (e.target.classList.contains('thumb-indicator')) {
+    dragging = true;
+    thumbIndicator.classList.add('focus');  
+  }
+});
+
+document.addEventListener('mousemove', function(e) {
+
+  if (dragging) {
+      console.log('moving', e)
+    if (e.clientX < sliderContainerStart) {
+      translate = 0;
+    } else if (e.clientX > sliderContainerWidth + sliderContainerStart) {
+      translate = sliderContainerWidth;
+    } else {
+      translate =  e.clientX - sliderContainer.offsetLeft;  
+    }
+    
+    thumb.style.transform = 'translate(-50%) translate(' + translate + 'px)';
+    trackProgress.style.transform = 'scaleX(' + translate / sliderContainerWidth +  ')'
+  }
+});
+
+function setPercentage() {
+   thumb.style.transform = 'translate(-50%) translate(' + percentage/100 * sliderContainerWidth + 'px)';
+  trackProgress.style.transform = 'scaleX(' + percentage/100 +  ')';
+}
+
+function init() {
+ setPercentage(); 
+}
+
+init();
+
+document.addEventListener('mouseup', function(e) {
+  dragging = false;
+  thumbIndicator.classList.remove('focus');
+});
